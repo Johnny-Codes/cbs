044845da4d565d8862592b5bc25d800ae92acd13
paul <johnspaulm@gmail.com>
2023-07-10 10:53:09 -0500

    netlify

--- cbs/settings.py
+++ cbs/settings.py
@@ -15,18 +15,50 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+debug_env = os.getenv("DEBUG_ENV")
+print(">>>>>>>>>>>>>>>>>>> debug", debug_env)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--q)!-8&g(04ckh98@z!nu6j6ckj_vvc^*l*)@iqzgd--1tmppu"
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+# SECURITY WARNING: don't run with debug turned on in production!]
+if debug_env == "development":
+    DEBUG = True
+    SECRET_KEY = os.environ.get("DEV_SECRET_KEY")
+    ALLOWED_HOSTS = ["localhost"]
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_NAME"),
+            "USER": os.environ.get("POSTGRES_USER"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+            "HOST": "db",
+            "PORT": 5432,
+        }
+    }
+else:
+    DEBUG = False
+    SECRET_KEY = os.environ.get("PROD_SECRET_KEY")
+    ALLOWED_HOSTS = []
+    CSRF_COOKIE_SECURE = True
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_SSL_REDIRECT = True
+    db_name = os.getenv("POSTGRES_NAME")
+    db_user = os.getenv("POSTGRES_USER")
+    db_pw = os.getenv("POSTGRES_PASSWORD")
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": db_name,
+            "USER": db_user,
+            "PASSWORD": db_pw,
+            "HOST": "db",
+            "PORT": 5432,
+        }
+    }
 
 
 # Application definition
@@ -86,16 +118,16 @@ WSGI_APPLICATION = "cbs.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_NAME"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": "db",
-        "PORT": 5432,
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.environ.get("POSTGRES_NAME"),
+#         "USER": os.environ.get("POSTGRES_USER"),
+#         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+#         "HOST": "db",
+#         "PORT": 5432,
+#     }
+# }
 
 
 # Password validation
