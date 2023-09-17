dfac50a95a5daf23c7aef7e8b375426c065ebfd2
Paul Johns <pauljohns@Pauls-MBP.lan>
2023-09-17 18:36:50 -0500

    added button and store

--- frontend/src/salesinvoice/SalesCustomer.tsx
+++ frontend/src/salesinvoice/SalesCustomer.tsx
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react";
+import { useGetAllCustomersQuery } from "../customers/api/customers";
+
 const SalesCustomer = () => {
+  const { data: allCustomers, isLoading: allCustomersLoading } =
+    useGetAllCustomersQuery("");
+  const [customers, setCustomers] = useState();
+
+  useEffect(() => {
+    if (allCustomers) {
+      setCustomers(allCustomers);
+    }
+  }, [allCustomers, setCustomers]);
+  if (allCustomersLoading) return <h1>Loading...</h1>;
+  console.log("all customers", allCustomers);
+
   return (
     <div>
       <h1>Sales Customer</h1>
+      {customers?.map((customer) => (
+        <div key={customer.id}>
+          {customer.first_name} {customer.last_name}
+        </div>
+      ))}
     </div>
   );
 };
