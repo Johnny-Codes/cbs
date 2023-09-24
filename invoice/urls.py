f7f913d132d3ce16f24e1b02706c5b593754e82d
Paul Johns <pauljohns@Pauls-MacBook-Pro.local>
2023-09-24 17:48:08 -0500

    complete sales invoice for customer view

--- invoice/urls.py
+++ invoice/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
-from .views import SalesInvoiceSerializerView
+from .views import (
+    SalesInvoiceSerializerView,
+    SalesInvoiceForCustomerView,
+)
 
 urlpatterns = [
-    path("sales/invoice/", SalesInvoiceSerializerView.as_view(), name="sales_invoice"),
+    path(
+        "sales/invoice/",
+        SalesInvoiceSerializerView.as_view(),
+        name="sales_invoice",
+    ),
+    path(
+        "sales/invoice/customer/<int:customer_id>",
+        SalesInvoiceForCustomerView.as_view(),
+        name="sales_invoice_for_customer",
+    ),
 ]
